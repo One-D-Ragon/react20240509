59d4b87e719687da7633fb595da1a3cb2d448adc
1dragon <dnjsdyd159@naver.com>
2024-05-09 14:47:58 +0900

    javascript object

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-function App() {
-  // javascript review
+// rsf로 만든다
+import React from "react";
 
-  // javascript data type
-  let a; // 값이 할당되지 않으면 undefined
-  console.log("a", typeof a);
-  let b = "문자열 타입"; // string
-  console.log("b", typeof b);
-  let c = 3; // number
-  console.log("c", typeof c);
-  let d = 3.14; // number
-  console.log("d", typeof d);
-  let e = true; // boolean
-  console.log("e", typeof e);
-  // typeof 연산자 : 타입을 알 수 있음
+function App(props) {
+  // object : {}로 만듬
+  // {} 안에 property명 : property값 쌍들이 작성됨
 
-  let f = {}; // object
-  // 중괄호 안에 프로퍼티와 프로퍼티 값을 넣을 수 있다
-  console.log("f", typeof f);
+  let a = {};
+  let b = { name: "흥민" };
+  let c = { address: "신촌", city: "서울", country: "한국" }; // 각 프로퍼티는 콤마(,)로 구분함
 
-  let g = function () {}; // function
-  console.log("g", typeof g);
-  g = {};
-  console.log("g", typeof g); // 자바스크립트에서는 값이 들어갈때 변수의 타입이 결정된다
+  console.log("a", a);
+  console.log("b", b);
+  console.log("c", c);
 
   return <div></div>;
 }
