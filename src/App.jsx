510491ba6c4e8f8bac2442de86975f918fcf015b
1dragon <dnjsdyd159@naver.com>
2024-05-09 15:31:33 +0900

    필요없는 코드 지움

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,6 @@
 import React from "react";
 
 function App(props) {
-  // object
-  const a = {
-    name: "John",
-  };
-
-  const b = a;
-  console.log(a === b); // true
-
-  console.log("a.name", a.name); // John
-  console.log("b.name", b.name); // John
-
-  b.name = "Jane"; // 객체는 참조값을 저장한다
-  console.log(a === b); // true
-
-  console.log("a.name", a.name); // Jane
-  console.log("b.name", b.name); // Jane
-
   return <div></div>;
 }
 
