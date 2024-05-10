0c6d40b22e298504ef3f9748c1ab6b9a4ae4796a
1dragon <dnjsdyd159@naver.com>
2024-05-10 14:14:02 +0900

    props(properties)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
+// props : properties
+// <MyButton name ="클릭3" />
+// props : { name: "클릭3" } => 중괄호 안의 객체가 props에 들어감(name 프로퍼티가 들어가있음)
+function MyButton(props) {
+  return (
+    <button
+      style={{
+        padding: "10px",
+        backgroundColor: "green",
+        color: "white",
+      }}
+    >
+      {props.name}
+    </button>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭1
+      </button>
+      <br />
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "blue",
+          color: "white",
+        }}
+      >
+        클릭2
+      </button>
+      <br />
+      {/*사용하는 곳에서 프로퍼티를 넘겨줄 수 있다*/}
+      <MyButton name="클릭3" />
+      <br />
+      <MyButton name="클릭4" />
+    </div>
+  );
 }
 
 export default App;
