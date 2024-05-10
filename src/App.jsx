1f094de8ef0bf77c4aed9bb3d7473341c478d531
1dragon <dnjsdyd159@naver.com>
2024-05-10 16:09:24 +0900

    code reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
 import React from "react";
 
 function App(props) {
-  // style props
-
-  return (
-    <div>
-      {/* 값이 객체여서 중괄호를 쓴다 */}
-      <div
-        style={{
-          color: "blue",
-          padding: "10px",
-          border: "2px dotted green",
-          textAlign: "center",
-          backgroundColor: "aqua",
-        }}
-      >
-        Lorem ipsum dolor.
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
