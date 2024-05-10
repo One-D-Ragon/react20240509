6cab01d27ffe139ca9b01d9b33b9a9ec53d1d931
1dragon <dnjsdyd159@naver.com>
2024-05-10 14:22:45 +0900

    필요없는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,7 @@
 import React from "react";
 
-function MyComp(props) {
-  console.log(props);
-  return (
-    <div>
-      <p>name : {props.name}</p>
-      <p>age : {props.age}</p>
-      <p>address : {props.address}</p>
-      <p>city : {props.city}</p>
-      <p>country : {props.country}</p>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name="son" age={33} address="seoul" />{" "}
-      {/* name, age, address 프로퍼티가 있는 객체가 만들어짐 */}
-      <MyComp city="busan" country="USA" />{" "}
-      {/* city,country 프로퍼티가 있는 객체가 만들어짐 */}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
