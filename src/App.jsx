d66a4af1c1c34e901aa73b7e9748f316731f45af
1dragon <dnjsdyd159@naver.com>
2024-05-10 16:08:51 +0900

    style props은 객체로
    css property 명은lowerCamelCase로

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // style props
+
+  return (
+    <div>
+      {/* 값이 객체여서 중괄호를 쓴다 */}
+      <div
+        style={{
+          color: "blue",
+          padding: "10px",
+          border: "2px dotted green",
+          textAlign: "center",
+          backgroundColor: "aqua",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
+    </div>
+  );
 }
 
 export default App;
