bc17110969392198572e29467e57b3474dc4cd0e
1dragon <dnjsdyd159@naver.com>
2024-05-13 11:11:47 +0900

    default export

---
+++ src/MyValues.jsx
@@ -0,0 +1,9 @@
+// named export
+export const v1 = "son";
+
+// default export
+// 한 파일에서 하나만 가능
+// 이름을 안써도 된다
+// string, number, function, object, array 아무거나 와도 된다
+export default "lee";
+// export default "kim"; // xxx
