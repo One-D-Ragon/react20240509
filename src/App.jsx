e9af5ed87f5290021ed38d6df496b3efc181545d
1dragon <dnjsdyd159@naver.com>
2024-05-13 11:02:50 +0900

    chakra ui component 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Badge, Button, ChakraProvider } from "@chakra-ui/react";
+
+// node_modules에 외부 컴포넌트들이 설치됨
+// 자동으로 import 된다
+// 외부 컴포넌트들을 가져와서 쓸 수 있다
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"blue"}>클릭</Button>
+        <Button colorScheme={"green"}>
+          알림<Badge>new</Badge>
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
