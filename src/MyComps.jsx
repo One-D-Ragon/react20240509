8e9cf13de4c3bab6b9e012c953dd6616d165b7c6
1dragon <dnjsdyd159@naver.com>
2024-05-13 10:26:25 +0900

    React Component의 import, export

--- src/MyComps.jsx
+++ src/MyComps.jsx
@@ -1,3 +1,21 @@
 export const myStr = "korea";
 export const myAddress = "seoul";
 export const value3 = 99;
+// 객체
+const value4 = {};
+// 배열
+const value5 = [];
+// 이름있는 함수
+export function action1() {
+  return null;
+}
+// 이름없는 함수
+export const action2 = function () {
+  return null;
+};
+// arrow function
+export const action3 = () => null;
+
+export function MyComp() {
+  return <div>Lorem.</div>;
+}
