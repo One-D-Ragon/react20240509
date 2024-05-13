874f2e4107dbe2b444937b8ed668a33f3520437b
1dragon <dnjsdyd159@naver.com>
2024-05-13 09:23:27 +0900

    children prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+function MyComp({ name, age, children }) {
+  console.log("name", name);
+  console.log("age", age);
+  console.log("children", children);
+  return (
+    <div>
+      <div>name : {name}</div>
+      <div>age : {age}</div>
+      <div>children : {children}</div>
+    </div>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp name={"son"} age={33}>
+        <p>Lorem ipsum dolor.</p>
+        {/* content는 children prop */}
+      </MyComp>
+      <MyComp name={"lee"} age={22}>
+        <ul>
+          <li>Lorem.</li>
+          <li>Quasi.</li>
+        </ul>
+      </MyComp>
+    </div>
+  );
 }
 
 export default App;
