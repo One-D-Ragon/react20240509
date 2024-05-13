ebedf4a928ed3128802f9e2247d37a14b135c3fa
1dragon <dnjsdyd159@naver.com>
2024-05-13 11:43:58 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val1, setVal1] = useState(0);
-  // 상태가 바뀔때 다시 그린다 = 함수를 다시 호출한다
-
-  let val2 = 0;
-
-  function updateVal1() {
-    setVal1(val1 + 1); // 상태를 업데이트
-  }
-
-  function updateVal2() {
-    val2++; // 변수를 업데이트
-  }
-
-  console.log("다시 호출됨");
-
-  return (
-    <div>
-      <button onClick={updateVal1}>val1 {val1}</button>
-      <button onClick={updateVal2}>val2 {val2}</button>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
