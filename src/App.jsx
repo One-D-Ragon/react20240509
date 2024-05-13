eec76012c613a4ddf33e0414853996185b149ad9
1dragon <dnjsdyd159@naver.com>
2024-05-13 12:03:26 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import React, { useState } from "react";
-
-function MyComp() {
-  const [val, setVal] = useState(0); // 초기값은 0(이전 상태)
-  console.log("다시 그려짐");
-  return (
-    <div>
-      <button onClick={() => setVal(1)}>클릭 {val}</button>
-      {/* 바뀐 상태, 기존 상태와 같은 값을 가지면 다시 그려지지 않는다 */}
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  return (
-    <div>
-      <MyComp />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
