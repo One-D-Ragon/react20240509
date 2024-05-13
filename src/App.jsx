7f22cac5f3d837a9680ef04086e08ffd2524c515
1dragon <dnjsdyd159@naver.com>
2024-05-13 11:47:12 +0900

    상태가 바뀔 때만 다시 그림

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  const [val, setVal] = useState(0); // 초기값은 0(이전 상태)
+  console.log("다시 그려짐");
+  return (
+    <div>
+      <button onClick={() => setVal(1)}>클릭 {val}</button>
+      {/* 바뀐 상태, 기존 상태와 같은 값을 가지면 다시 그려지지 않는다 */}
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
