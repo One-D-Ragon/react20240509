c432e767ff8c27219ceadf28350c6478a74efabd
1dragon <dnjsdyd159@naver.com>
2024-05-13 09:31:58 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, children }) {
-  console.log("name", name);
-  console.log("age", age);
-  console.log("children", children);
-  return (
-    <div>
-      <div>name : {name}</div>
-      <div>age : {age}</div>
-      <div>children : {children}</div>
-    </div>
-  );
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={33}>
-        <p>Lorem ipsum dolor.</p>
-        {/* content는 children prop */}
-      </MyComp>
-      <MyComp name={"lee"} age={22}>
-        <ul>
-          <li>Lorem.</li>
-          <li>Quasi.</li>
-        </ul>
-      </MyComp>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
