f737ebd06ad696ddf78097583ec5d046c620755b
1dragon <dnjsdyd159@naver.com>
2024-05-14 10:27:47 +0900

    "lift state up"으로 컴포넌트의 상태 유지 가능

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ count, setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>UP</button>
+      <p>{count}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [display, setDisplay] = useState(true);
+  const [count, setCount] = useState(0);
+
+  // lift state up : 상위 컴포넌트에 하위 컴포넌트의 상태를 올린다
+  // 하위 컴포넌트의 상태 유지가 가능하다
+  // props, setCount
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={display}
+        onChange={(e) => setDisplay(e.target.checked)}
+      />
+      {display && <MyBox count={count} setCount={setCount} />}
+    </div>
+  );
 }
 
 export default App;
