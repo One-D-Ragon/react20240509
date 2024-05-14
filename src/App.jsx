fff1897c7933ef9d9f48d9ad90f03bc1024d89cf
1dragon <dnjsdyd159@naver.com>
2024-05-14 12:12:04 +0900

    코드 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
+import React from "react";
 
-// step1 : create context
-const MessageContext = createContext("");
-
-function MyBox() {
-  // step2 : use the context
-  // prop drilling이 일어난 코드를 지운다
-  const message = useContext(MessageContext);
-  return <div>{message}</div>;
-}
-
-function MySection() {
-  return <MyBox />;
-}
-
-function MyContainer() {
-  return <MySection />;
-}
-
-// context : prop drilling 없이 값을 전달하는 방법
-// https://react.dev/learn/passing-data-deeply-with-context
-// # context 과용하지 말 것
-// 1. prop 전달 붙처 시작할 것
-// 2. 주로 theme, 현재 계정정보, routing 등에 사용됨
 function App(props) {
-  const [message, setMessage] = useState("");
-
-  return (
-    <div>
-      <input type="text" onChange={(e) => setMessage(e.target.value)} />
-      <p>{message}</p>
-      <hr />
-      {/* step3 : provide the context */}
-      <MessageContext.Provider value={message}>
-        <MyContainer />
-      </MessageContext.Provider>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
