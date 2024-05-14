828b3fc539cf205ff893cdd144d79d715e54012e
1dragon <dnjsdyd159@naver.com>
2024-05-14 10:39:43 +0900

    코드 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-
-function MyBox({ count, setCount }) {
-  return (
-    <div>
-      <button onClick={() => setCount(count + 1)}>UP</button>
-      <p>{count}</p>
-    </div>
-  );
-}
+import React from "react";
 
 function App(props) {
-  const [display, setDisplay] = useState(true);
-  const [count, setCount] = useState(0);
-
-  // lift state up : 상위 컴포넌트에 하위 컴포넌트의 상태를 올린다
-  // 하위 컴포넌트의 상태 유지가 가능하다
-  // props, setCount
-
-  return (
-    <div>
-      <input
-        type="checkbox"
-        checked={display}
-        onChange={(e) => setDisplay(e.target.checked)}
-      />
-      {display && <MyBox count={count} setCount={setCount} />}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
