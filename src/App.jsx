2425a6eea2ca85868f593c0a8cfbcf4b39a81dc2
1dragon <dnjsdyd159@naver.com>
2024-05-16 09:36:17 +0900

    /api/someurl의 get, post ajax 요청 버튼 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    // ajax get 요청
+    axios.get("/api/someurl");
+  }
+
+  function handleClickButton2() {
+    // ajax post 요청
+    axios.post("/api/someurl");
+  }
+
+  return (
+    <div>
+      <button onClick={handleClickButton1}>get 요청</button>
+      <button onClick={handleClickButton2}>post 요청</button>
+    </div>
+  );
 }
+
+// axios에서 []안의 내용은 생략 가능
 
 export default App;
