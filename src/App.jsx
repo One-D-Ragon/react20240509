d62805e7d1c680bbb1f7bccd5686093f076dc105
1dragon <dnjsdyd159@naver.com>
2024-05-16 11:23:44 +0900

    객체를 json으로 직렬화해서 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClick1() {
+    // json 데이터는 get 방식 요청이 불가능
+
+    const obj1 = { name: "son", age: 44 };
+    // const json1 = JSON.stringify(obj1);
+
+    // axios가 두번째 파라미터가 객체면 직렬화 해줌
+    axios.post("/api/main41/sub1", obj1);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick1}>요청(json 데이터 포함)</button>
+    </div>
+  );
 }
 
 export default App;
