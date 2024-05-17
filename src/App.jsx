382d0d8d145cb6e5c565ae41ebefeeb82614c3d0
1dragon <dnjsdyd159@naver.com>
2024-05-17 15:10:32 +0900

    상위 경로에 element 추가시 하위 경로는 Outlet으로 출력됨

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "spring",
+    element: (
+      <div>
+        spring root page
+        <Outlet />{" "}
+        {/* 하위 경로에 있는 것을 보여준다. 상위 경로는 안바뀌고 하위 경로만 바뀐다 */}
+      </div>
+    ),
+    children: [
+      { path: "api", element: <div>api page</div> },
+      { path: "doc", element: <div>doc page</div> },
+      { path: "learn", element: <div>learn page</div> },
+    ],
+  },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
