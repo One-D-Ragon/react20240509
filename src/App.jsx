cf2ba28b7adc3db360cf50b35acb56d12af58508
1dragon <dnjsdyd159@naver.com>
2024-05-20 09:26:56 +0900

    차크라 ui 버튼

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <div>
+        <Button colorScheme={"gray"}> gray </Button>
+        <Button colorScheme={"red"}>Lorem.</Button>
+        <Button colorScheme={"orange"}>Officia!</Button>
+        <Button colorScheme={"yellow"}>Dolor.</Button>
+        <Button colorScheme={"green"}>Pariatur.</Button>
+        <Button colorScheme={"teal"}>Similique.</Button>
+        <Button colorScheme={"blue"}>Dolorem?</Button>
+        <Button colorScheme={"cyan"}>Atque.</Button>
+        <Button colorScheme={"purple"}>Ad?</Button>
+        <Button colorScheme={"pink"}>Cupiditate!</Button>
+        <hr />
+        <Button colorScheme={"blue"} isLoading={true}>
+          Lorem.
+        </Button>
+        <hr />
+        <Button colorScheme={"blue"} variant={"solid"}>
+          Lorem.
+        </Button>
+        <Button colorScheme={"blue"} variant={"outline"}>
+          Nobis.
+        </Button>
+        <Button colorScheme={"blue"} variant={"ghost"}>
+          Nemo!
+        </Button>
+        <Button colorScheme={"blue"} variant={"link"}>
+          Hic.
+        </Button>
+      </div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
