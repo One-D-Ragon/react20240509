6d769a70a5e52fa5a05905be6006cea97141c44e
1dragon <dnjsdyd159@naver.com>
2024-05-20 10:26:40 +0900

    sx : 스타일 덮어쓰기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Button sx={{ color: "orange" }} colorScheme={"blue"}>
+        Lorem.
+      </Button>
+      <div></div>
+    </ChakraProvider>
+  );
 }
 
 export default App;
