b7ececea305c97b72c5f29ba7572c42f61084ed4
1dragon <dnjsdyd159@naver.com>
2024-05-20 10:20:00 +0900

    코드 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <ChakraProvider>
-      <Box bgColor={"tomato"} w={"500px"}>
-        Lorem ipsum dolor.
-      </Box>
-      {/* md: 768px을 넘어가면 500px로 */}
-      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
-        Lorem ipsum dolor.
-      </Box>
+      <Box></Box>
     </ChakraProvider>
   );
 }
