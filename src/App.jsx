1cc01dedee4420d92012723d2caeaa41ea67bf39
1dragon <dnjsdyd159@naver.com>
2024-05-20 10:17:30 +0900

    반응형 스타일

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <ChakraProvider>
+      <Box bgColor={"tomato"} w={"500px"}>
+        Lorem ipsum dolor.
+      </Box>
+      {/* md: 768px을 넘어가면 500px로 */}
+      <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+        Lorem ipsum dolor.
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
