8dbaf8d4c15eb2a5f701fd696481748a77a7855b
1dragon <dnjsdyd159@naver.com>
2024-05-20 15:12:05 +0900

    file 전송
    method=post
    
    encType="multipart/form-data"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      {/* file 보낼때는 post 방식과 encType을 꼭 써줘야한다*/}
+      <form action="/path" method="post" encType="multipart/form-data">
+        이름 <input type="text" name="name" />
+        <br />
+        <input type="file" name="file" />
+        <br />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default App;
